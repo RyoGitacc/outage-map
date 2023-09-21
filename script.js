b19dbb9b79acf051093ac3cdd4ccde273befb7ce
William Poon <willphk@outlook.com>
2023-09-21 15:20:20 -0400

    [Update] Added Heatmap

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let userLatitude = 43.65107;
 let userLongitude = -79.347015;
 
-if ('geolocation' in navigator) {
+if ("geolocation" in navigator) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
       userLatitude = position.coords.latitude;
@@ -10,25 +10,25 @@ if ('geolocation' in navigator) {
       console.log(` User's Lat: ${userLatitude} User's Lng: ${userLongitude}`);
     },
     function (error) {
-      console.error('Error occurred: ', error.message);
-      alert('Unable to get location. Please enable location services.');
+      console.error("Error occurred: ", error.message);
+      alert("Unable to get location. Please enable location services.");
     }
   );
 } else {
-  console.error('Geolocation not supported.');
-  alert('Your browser does not support geolocation.');
+  console.error("Geolocation not supported.");
+  alert("Your browser does not support geolocation.");
 }
 
 mapboxgl.accessToken =
-  'pk.eyJ1Ijoid2hhdG5vd21hcCIsImEiOiJjbGw0Nnk1aTkwMXIxM2VwMGpiN3RmZ3Y5In0.O5vq93APpSPPQgPHc9VC6g';
+  "pk.eyJ1Ijoid2hhdG5vd21hcCIsImEiOiJjbGw0Nnk1aTkwMXIxM2VwMGpiN3RmZ3Y5In0.O5vq93APpSPPQgPHc9VC6g";
 
 const map = new mapboxgl.Map({
-  container: 'map',
+  container: "map",
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-  style: 'mapbox://styles/mapbox/dark-v10',
+  style: "mapbox://styles/mapbox/dark-v10",
   center: [userLongitude, userLatitude], //Setting lat lng
   zoom: 11,
-  projection: 'globe', // starting projection
+  projection: "globe", // starting projection
 });
 
 // TODO: Figure out how to show live pin
@@ -63,70 +63,158 @@ const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(popupContent);
 
 // Attach the popup to a marker or any other Mapbox GL JS object you want
 
-// Create a default Marker and add it to the map.
-new mapboxgl.Marker({
-  color: '#633BA5',
-  scale: 1,
-})
-  .setLngLat([-79.40371741661295, 43.7053100335487])
-  .setPopup(popup) // sets a popup on this marker
-  .addTo(map);
-
 // map.setCenter([]) // Center the map anytime the user loads it
 
 // Adding the boundaries of toronto
-map.on('load', () => {
+map.on("load", () => {
   map.setFog({});
-  map.addSource('gta-borough-boundaries', {
-    type: 'geojson',
-    data: 'data/gta-borough-boundaries.geojson',
+  map.addSource("earthquakes", {
+    type: "geojson",
+    data: "./data/powerOutage.geojson", // Replace with your data URL
+  });
+  map.addSource("gta-borough-boundaries", {
+    type: "geojson",
+    data: "data/gta-borough-boundaries.geojson",
   });
 
   map.addLayer({
-    id: 'gta-borough-boundaries-fill',
-    type: 'fill',
-    source: 'gta-borough-boundaries',
+    id: "gta-borough-boundaries-fill",
+    type: "fill",
+    source: "gta-borough-boundaries",
     paint: {
-      'fill-color': '#633BA5',
-      'fill-opacity': 0.1,
+      "fill-color": "#633BA5",
+      "fill-opacity": 0.1,
     },
   });
 
   //   Outlining the different areas in toronto
   map.addLayer({
-    id: 'gta-borough-boundaries-line',
-    type: 'line',
-    source: 'gta-borough-boundaries',
+    id: "gta-borough-boundaries-line",
+    type: "line",
+    source: "gta-borough-boundaries",
     paint: {
-      'line-color': 'white',
-      'line-width': 1,
-      'line-opacity': 0.5,
+      "line-color": "white",
+      "line-width": 1,
+      "line-opacity": 0.5,
     },
   });
+
+  map.addLayer(
+    {
+      id: "earthquakes-heat",
+      type: "heatmap",
+      source: "earthquakes",
+      maxzoom: 9,
+      paint: {
+        // Increase the heatmap weight based on frequency and property magnitude
+        "heatmap-weight": [
+          "interpolate",
+          ["linear"],
+          ["get", "mag"],
+          0,
+          0,
+          6,
+          1,
+        ],
+        // Increase the heatmap color weight weight by zoom level
+        // heatmap-intensity is a multiplier on top of heatmap-weight
+        "heatmap-intensity": ["interpolate", ["linear"], ["zoom"], 0, 1, 9, 3],
+        // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+        // Begin color ramp at 0-stop with a 0-transparancy color
+        // to create a blur-like effect.
+        "heatmap-color": [
+          "interpolate",
+          ["linear"],
+          ["heatmap-density"],
+          0,
+          "rgba(33,102,172,0)",
+          0.2,
+          "rgb(103,169,207)",
+          0.4,
+          "rgb(209,229,240)",
+          0.6,
+          "rgb(253,219,199)",
+          0.8,
+          "rgb(239,138,98)",
+          1,
+          "rgb(178,24,43)",
+        ],
+        // Adjust the heatmap radius by zoom level
+        "heatmap-radius": ["interpolate", ["linear"], ["zoom"], 0, 2, 9, 20],
+        // Transition from heatmap to circle layer by zoom level
+        "heatmap-opacity": ["interpolate", ["linear"], ["zoom"], 7, 1, 9, 0],
+      },
+    },
+    "waterway-label"
+  );
+
+  map.addLayer(
+    {
+      id: "earthquakes-point",
+      type: "circle",
+      source: "earthquakes",
+      minzoom: 7,
+      paint: {
+        // Size circle radius by earthquake magnitude and zoom level
+        "circle-radius": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          7,
+          ["interpolate", ["linear"], ["get", "mag"], 1, 1, 6, 4],
+          16,
+          ["interpolate", ["linear"], ["get", "mag"], 1, 5, 6, 50],
+        ],
+        // Color circle by earthquake magnitude
+        "circle-color": [
+          "interpolate",
+          ["linear"],
+          ["get", "mag"],
+          1,
+          "rgba(33,102,172,0)",
+          2,
+          "rgb(103,169,207)",
+          3,
+          "rgb(209,229,240)",
+          4,
+          "rgb(253,219,199)",
+          5,
+          "rgb(239,138,98)",
+          6,
+          "rgb(178,24,43)",
+        ],
+        "circle-stroke-color": "white",
+        "circle-stroke-width": 1,
+        // Transition from heatmap to circle layer by zoom level
+        "circle-opacity": ["interpolate", ["linear"], ["zoom"], 7, 0, 8, 1],
+      },
+    },
+    "waterway-label"
+  );
 });
 
 function getUserLocationAndRedirect(callback) {
-  if ('geolocation' in navigator) {
+  if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(
       function (position) {
         let userLatitude = position.coords.latitude;
         let userLongitude = position.coords.longitude;
 
         // Event coordinates, which will be dynamically called from the database.
-        let eventLatitude = '43.7053100335487';
-        let eventLongitude = '-79.40371741661295';
+        let eventLatitude = "43.7053100335487";
+        let eventLongitude = "-79.40371741661295";
 
         // Call the redirection function with the coordinates
         callback(eventLatitude, eventLongitude, userLatitude, userLongitude);
       },
       function (error) {
-        console.error('Error occurred: ', error.message);
-        alert('Unable to get location. Please enable location services.');
+        console.error("Error occurred: ", error.message);
+        alert("Unable to get location. Please enable location services.");
       }
     );
   } else {
-    console.error('Geolocation not supported.');
-    alert('Your browser does not support geolocation.');
+    console.error("Geolocation not supported.");
+    alert("Your browser does not support geolocation.");
   }
 }
 
@@ -137,7 +225,7 @@ function redirectToGoogle(
   userLongitude
 ) {
   let googleMapsURL = `https://www.google.com/maps/dir/?api=1&origin=${userLatitude},${userLongitude}&destination=${eventLatitude},${eventLongitude}&travelmode=driving`;
-  window.open(googleMapsURL, '_blank');
+  window.open(googleMapsURL, "_blank");
 }
 
 function redirectToApple(
@@ -147,7 +235,7 @@ function redirectToApple(
   userLongitude
 ) {
   let appleMapsURL = `http://maps.apple.com/?saddr=${userLatitude},${userLongitude}&daddr=${eventLatitude},${eventLongitude}`;
-  window.open(appleMapsURL, '_blank');
+  window.open(appleMapsURL, "_blank");
 }
 
 function redirectToWaze(
@@ -157,5 +245,5 @@ function redirectToWaze(
   userLongitude
 ) {
   let wazeURL = `https://waze.com/ul?ll=${eventLatitude},${eventLongitude}&navigate=yes&zoom=17&coords=${userLatitude},${userLongitude}`;
-  window.open(wazeURL, '_blank');
+  window.open(wazeURL, "_blank");
 }
